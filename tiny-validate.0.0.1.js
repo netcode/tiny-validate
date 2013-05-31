740657a92cda6b98fbcafa136725361fa5e9405a
eslam salem <eslam@netcode.me>
2013-05-31 22:33:43 +0200

    add functions

--- tiny-validate.0.0.1.js
+++ tiny-validate.0.0.1.js
@@ -7,13 +7,30 @@
 				'required':'this field is required',
 				'number':'this field must be number'
 			},
+
 		}, options );
+
+        var processFunctions : {
+        	'required':function(val){
+        		return (val != '')
+        	},
+        	'number':function(val){
+
+        	},
+        	'email':function(val){
+        		return /e@e.com/.exec(val);
+        	},
+        	'match':function(val,nextVal){
+        		return (val == nextVal);
+        	}
+        }
 		
 		this.each(function(){
 			var Obj = $(this);
 			Obj.on('submit',function(){
 				var requiredFields = Obj.find('.required');
-				
+				var numberFields = obj.find('.number');
+				var emailFields = obj.find('.email');
 			})
 		});
 	};		
